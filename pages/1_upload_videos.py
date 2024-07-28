0669cec9ec196440644a7d4f9342118a9984bc50
danikagupta <74515001+danikagupta@users.noreply.github.com>
2024-07-28 15:24:13 +0000

    Upload video list

--- pages/1_upload_videos.py
+++ pages/1_upload_videos.py
@@ -5,18 +5,30 @@ show_navigation()
 
 
 from generate_transcript import transcribe_session
-from session_info import get_update_session_info
+from session_info import get_update_session_info, extract_video_id
 
 def upload_one_video():
-    st.write("Upload Video URL (e.g. https://youtu.be/vgYi3Wr7v_g)")
-    #yt_url = st.text_input("Youtube Video URL")
-    #if yt_url is not None:
-    if yt_url := st.text_input("Youtube Video URL"):
+    st.markdown("# Upload Video URL\n Example: https://youtu.be/vgYi3Wr7v_g)")
+    if yt_url := st.text_input(" "):
         st.write("Video upload start")
         info=get_update_session_info(yt_url)
         st.table(info)
-    else:
-        st.write("Upload a video file")
+
+def upload_video_list_file():
+    st.markdown("# Upload Video List File")
+    if video_list_file := st.file_uploader("Upload Video List File", type=["csv"]):
+        st.write("Video list file upload start")
+        video_list=[]
+        for line in video_list_file.getvalue().decode().split("\n"):
+            yt_url=line.strip()
+            if yt_url.lower().startswith("http"):
+                st.write(f"Processing :{yt_url}:")
+                info=get_update_session_info(yt_url)
+                video_list.append(info)
+        st.table(video_list)
 
 st.title("🎈 Youtube Review Page 2")
+st.divider()
 upload_one_video()
+st.divider()
+upload_video_list_file()
--- streamlit_app.py
+++ streamlit_app.py
@@ -7,29 +7,3 @@ show_navigation()
 st.title("🎈 Youtube Review")
 
 
-
-
-
-aaa="""
-tab1,tab2,tab3=st.tabs(["Upload Video","Review Video","See Analysis"])
-
-def tab_upload_videos():
-    st.write("Upload Video URL (e.g. https://youtu.be/vgYi3Wr7v_g)")
-    #yt_url = st.text_input("Youtube Video URL")
-    #if yt_url is not None:
-    if yt_url := st.text_input("Youtube Video URL"):
-        st.write("Video uploaded successfully")
-        transcript=transcribe_session(yt_url,st.secrets['ASSEMBLYAI_API_KEY'])
-        st.write(transcript)
-    else:
-        st.write("Upload a video file")
-
-with tab1:
-    tab_upload_videos()
-with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
-"""
