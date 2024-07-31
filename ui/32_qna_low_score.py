54d427c19fc6ea55db7cf44951782f004ef25c61
danikagupta <74515001+danikagupta@users.noreply.github.com>
2024-07-31 17:46:01 +0000

    Updated defaults

--- ui/32_qna_low_score.py
+++ ui/32_qna_low_score.py
@@ -11,14 +11,22 @@ from streamlit_app import authenticate
 
 st.markdown("# Low-scoring answers")
 
+exclude_list=[
+     "Were there any parts of the session where students seemed confused or needed further clarification? Were there any notable strengths or areas for improvement that could enhance future training sessions?",
+     "Did the teacher assign any homework during the class? If so, what was it?"
+]
+
 def main():
     values = st.slider("Define low range", 0, 10, (1, 6))
-    min_duration = st.number_input("Min duration in minutes", value=30)
+    min_duration = st.number_input("Min duration in minutes", value=10)
+    exclude_yn = st.checkbox("Exclude common low-scoring questions about homework assignment and students being confused", value=True)
     if st.button("Run analysis"):
         qnas=get_qna_range(values[0],values[1])
         dataset=[]
         for qna in qnas:
             qnad=qna['data']
+            if exclude_yn and qnad['question'] in exclude_list:
+                continue
             if qnad['duration']/60>=min_duration:
                 dataset.append({'Title':qnad['title'],'Score':qnad['score'],'Minutes':round(qnad['duration']/60),
                                 'Question':qnad['question'],'Answer':qnad['answer'],
